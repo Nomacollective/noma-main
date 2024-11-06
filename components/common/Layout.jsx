f767c6ff256d14fb0e779e531b9363d2aab8dc95
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2024-11-06 17:27:47 +0100

    Correct bug.

--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
     <>
       <script
         type="text/javascript"
-        charset="UTF-8"
+        charSet="UTF-8"
         src="//cdn.cookie-script.com/s/e28166a6cec87c8d411cf6eb7ada29f7.js"
       ></script>
       <GoogleTagManager gtmId="GTM-WRZZGKJ" />

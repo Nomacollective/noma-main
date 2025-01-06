0add39bd9baff33e5aa1c300e61c63a0f11660fa
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2025-01-06 19:20:57 +0100

    Fix bug with black lines on the video.

--- components/common/VideoPlayer.jsx
+++ components/common/VideoPlayer.jsx
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
 const VideoPlayer = ({ url }) => {
-  const [height, setHeight] = useState("600px");
-  const [width, setWidth] = useState("65%");
+  const [height, setHeight] = useState("580px");
+  const [width, setWidth] = useState("63%");
 
   useEffect(() => {
     const updateHeight = () => {
       if (window.innerWidth <= 768) {
-        setHeight("300px");
-        setWidth("85%");
+        setHeight("200px");
+        setWidth("90%");
       } else if (window.innerWidth <= 1200) {
-        setHeight("450px");
+        setHeight("330px");
       } else {
         setHeight("600px");
       }

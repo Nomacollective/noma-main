8ebf13d1250d37a07dfe4989ec4353cab7036179
RenMG00 <laurengian@gmail.com>
2025-04-30 12:37:51 -0600

    Additional change to resolve merge conflict

--- pages/team-retreats.js
+++ pages/team-retreats.js
@@ -16,7 +16,6 @@ const TeamRetreats = () => {
           heading="Team Retreats"
           text="Give your team a break and let us shoulder the burden of planning a memorable and inspiring corporate getaway"
           btn="REQUEST A QUOTE"/>
-        {/* FIXME: */}
         {/* <button onClick={() => visitPage()}>Visit Page Now</button>  */}
         <WhatToExpect />
         <WhyNoma />

dc68838d47bc44a61b95f15c96626e23cb0c5d2c
RenMG00 <laurengian@gmail.com>
2025-06-17 16:28:57 -0600

    Additional changes to profile meet styling

--- components/editions/ProfileMeet.jsx
+++ components/editions/ProfileMeet.jsx
@@ -13,13 +13,12 @@ const ProfileMeet = ({ managers = [] }) => {
           <FaciltiesIcons />
         </div>
 
-        <div className="flex flex-col gap-12">
+        <div className="grid gap-12 md:grid-cols-2">
           {managers.map((manager, index) => (
             <div
               key={manager?.sys?.id || index}
-              className="flex flex-col sm:flex-row gap-6 sm:gap-12 items-start"
+              className="flex flex-col sm:flex-row gap-6 items-center"
             >
-              {/* Left: Image and heading */}
               <div className="max-w-[265px] w-full text-center sm:text-left">
                 <h1 className="text-[#313131] font-Montserrat text-xl sm:text-[32px] font-extrabold leading-normal">
                   Meet {manager?.name}
@@ -27,17 +26,18 @@ const ProfileMeet = ({ managers = [] }) => {
                 <p className="text-[#313131] font-Montserrat text-sm sm:text-base font-normal leading-normal">
                   Your local Community Manager
                 </p>
-                <Image
-                  src={manager?.profileImage?.url}
-                  height={247}
-                  width={247}
-                  className="pt-4 rounded-full object-cover mx-auto sm:mx-0"
-                  alt={`Noma Collective community manager - ${manager?.name}`}
-                />
+                <div className="w-[247px] h-[247px] rounded-full overflow-hidden mx-auto sm:mx-0 pt-4">
+                  <Image
+                    src={manager?.profileImage?.url}
+                    alt={`Noma Collective community manager - ${manager?.name}`}
+                    width={247}
+                    height={247}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
               </div>
 
-              {/* Right: Description */}
-              <div className="sm:max-w-[540px] w-full flex flex-col gap-2 sm:gap-4 text-center sm:text-left text-[#313131] text-sm sm:text-base font-normal font-Montserrat leading-normal">
+              <div className="sm:max-w-[298px] w-full flex flex-col gap-2 sm:gap-4 text-center sm:text-left text-[#313131] text-sm sm:text-base font-normal font-Montserrat leading-normal">
                 {documentToReactComponents(manager?.description?.json)}
               </div>
             </div>

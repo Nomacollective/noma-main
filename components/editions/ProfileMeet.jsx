7d2176d660e6a30c078dff49ccad437c640502d1
RenMG00 <laurengian@gmail.com>
2025-06-17 16:22:53 -0600

    updated logic in profilemeet component for multiple managers

--- components/editions/ProfileMeet.jsx
+++ components/editions/ProfileMeet.jsx
@@ -13,12 +13,13 @@ const ProfileMeet = ({ managers = [] }) => {
           <FaciltiesIcons />
         </div>
 
-        <div className="grid gap-12 md:grid-cols-2">
+        <div className="flex flex-col gap-12">
           {managers.map((manager, index) => (
             <div
               key={manager?.sys?.id || index}
-              className="flex flex-col sm:flex-row gap-6 items-center"
+              className="flex flex-col sm:flex-row gap-6 sm:gap-12 items-start"
             >
+              {/* Left: Image and heading */}
               <div className="max-w-[265px] w-full text-center sm:text-left">
                 <h1 className="text-[#313131] font-Montserrat text-xl sm:text-[32px] font-extrabold leading-normal">
                   Meet {manager?.name}
@@ -29,13 +30,14 @@ const ProfileMeet = ({ managers = [] }) => {
                 <Image
                   src={manager?.profileImage?.url}
                   height={247}
-                  width={249}
+                  width={247}
                   className="pt-4 rounded-full object-cover mx-auto sm:mx-0"
                   alt={`Noma Collective community manager - ${manager?.name}`}
                 />
               </div>
 
-              <div className="sm:max-w-[298px] w-full flex flex-col gap-2 sm:gap-4 text-center sm:text-left text-[#313131] text-sm sm:text-base font-normal font-Montserrat leading-normal">
+              {/* Right: Description */}
+              <div className="sm:max-w-[540px] w-full flex flex-col gap-2 sm:gap-4 text-center sm:text-left text-[#313131] text-sm sm:text-base font-normal font-Montserrat leading-normal">
                 {documentToReactComponents(manager?.description?.json)}
               </div>
             </div>
